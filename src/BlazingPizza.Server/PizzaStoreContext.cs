19a36f2a928cedfc0dd1aba80ad85290b166c35e
Daniel Roth <daroth@microsoft.com>
2020-03-27 12:18:37 -0700

    Update authentication lab to use IdentityServer

--- src/BlazingPizza.Server/PizzaStoreContext.cs
+++ src/BlazingPizza.Server/PizzaStoreContext.cs
@@ -1,15 +1,15 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using IdentityServer4.EntityFramework.Options;
+using Microsoft.AspNetCore.ApiAuthorization.IdentityServer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
 
 namespace BlazingPizza.Server
 {
-    public class PizzaStoreContext : DbContext
+    public class PizzaStoreContext : ApiAuthorizationDbContext<PizzaStoreUser>
     {
-        public PizzaStoreContext()
-        {
-        }
-
-        public PizzaStoreContext(DbContextOptions options)
-            : base(options)
+        public PizzaStoreContext(
+            DbContextOptions options,
+            IOptions<OperationalStoreOptions> operationalStoreOptions) : base(options, operationalStoreOptions)
         {
         }
 
